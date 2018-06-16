7b08630353da7124b83f6e4db7ff3d361ea7a6d6
Justin McCormick <dmaxjm@gmail.com>
2018-06-16 17:13:41 -0500

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-wget https://github.com/dmaxjm/Aegeus/raw/master/update1.2.tar.gz
-tar -xzf update1.2.tar.gz
-systemctl stop Aegeus.service
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "[+] ${RED}Minivan AEG 1.2 Install Script${NC}"
+echo "[+] Downloading Update..."
+wget https://github.com/dmaxjm/Aegeus/raw/master/update1.2.tar.gz >/dev/null
+echo "[+] Extracting Update..."
+tar -xzf update1.2.tar.gz >/dev/null
+echo "[+] Stopping Service..."
+systemctl stop Aegeus.service >/dev/null 
+echo "[+] Updating Files" 
 cp ./update1.2/* /usr/local/bin/
-systemctl start Aegeus.service
-rm -rf ./update1.2
-
-
+echo "[+] Starting Service" 
+systemctl start Aegeus.service >/dev/null
+echo "[+] Cleaning Up"
+rm -rf ./update1.2* >/dev/null
+echo "[*] Updated Masternode to 1.2"
