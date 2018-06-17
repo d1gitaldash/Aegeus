e703c390c0dc25a8d889f99180108ff374a35f69
Justin McCormick <dmaxjm@gmail.com>
2018-06-17 09:00:23 -0500

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 echo -e "[+] ${RED}Minivan AEG 1.2 Install Script${NC}"
 echo "[+] Downloading Update..."
-wget https://www.dropbox.com/s/flafrvia36ahnyx/update1.2.tar.gz >/dev/null
+wget https://www.dropbox.com/s/qj2r12mhdiy0hn5/update1.2.tar.gz >/dev/null
 echo "[+] Extracting Update..."
 tar -xzf update1.2.tar.gz >/dev/null
 echo "[+] Stopping Service..."
